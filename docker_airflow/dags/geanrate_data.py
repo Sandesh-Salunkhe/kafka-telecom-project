4f0f14a1016fb271520aa6b57ba1ffdccd4c96b1
Sandesh Salunkhe <56861175+Sandesh-Salunkhe@users.noreply.github.com>
2022-04-15 12:27:21 +0530

    Update geanrate_data.py

--- docker_airflow/dags/geanrate_data.py
+++ docker_airflow/dags/geanrate_data.py
@@ -13,7 +13,7 @@ import os
 
 mydir=os.path.join(os.getcwd(),"dags/")
 def generate_data():
-    conn = create_engine("mysql://mysqluser:mysqlpw@13.233.122.201:3000/inventory") # connect to server
+    conn = create_engine("mysql://mysqluser:mysqlpw@15.206.88.175:3000/inventory") # connect to server
     engine = create_engine('sqlite:///telecom.db', echo = True)
     
     rec_count = 0
